24bf3e8ecb94a0a00d06b9418be4a8669eeaf11f
smilee1111 <muskankc2005@gmail.com>
2025-12-27 14:14:04 +0545

    create home page and dashboard dummy

--- bazar-frontend/app/(public)/layout.tsx
+++ bazar-frontend/app/(public)/layout.tsx
@@ -1,12 +1,3 @@
-import Header from "./_components/Header";
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <section>
-            <Header />
-            <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                {children}
-            </main>
-        </section>
-    );
+    return <>{children}</>;
 }
